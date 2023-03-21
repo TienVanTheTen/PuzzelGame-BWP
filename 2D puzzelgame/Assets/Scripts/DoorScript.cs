f1d6d78c1c63cea78ac97a6c632a86171651020c
TienVanTheTen <e.c.games@outlook.com>
2023-03-21 16:31:04 +0100

    TilesSet Automation, door fuctionaliteit, extra tiles gemaakt

--- 2D puzzelgame/Assets/Scripts/DoorScript.cs
+++ 2D puzzelgame/Assets/Scripts/DoorScript.cs
@@ -6,11 +6,33 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-
+    [SerializeField] private float closingDuration;
+    [SerializeField] private Vector2 closedPos;
+    [SerializeField] private Vector2 openPos;
+    
     public void OpenDoor()
     {
+        StopAllCoroutines();
+        StartCoroutine(MoveDoor(transform.position, openPos));
+        
+    }
+    public void CloseDoor()
+    {
+        
+        StopAllCoroutines();
+        StartCoroutine(MoveDoor(transform.position, closedPos));
+    }
+    private IEnumerator MoveDoor(Vector2 startPos, Vector2 endPos)
+    {
+        float time = 0f;
+        while (time <= 1.0)
+        {
+            time += Time.deltaTime / closingDuration;
+            transform.position = Vector2.Lerp(startPos, endPos, time);
+            yield return null;
 
-
+        }
+        
     }
 
 }
